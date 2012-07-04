576792bed3cd0c09103f97cef1358e8aa02d3b1f
mgawlista <mg@reqorder.com>
2012-07-04 17:41:50 +0200

    Moves routes to mount_rails_info. So that engine routing also works with the catch all route of applications.

--- config/routes.rb
+++ config/routes.rb
@@ -1,20 +1,3 @@
 Rails.application.routes.draw do
-  get '/rails/info' => 'rails_info/properties#index', as: 'rails_info'
-  
-  get '/rails/info/properties', to: 'rails_info/properties#index', as: 'rails_info_properties'
-  get '/rails/info/routes', to: 'rails_info/routes#index', as: 'rails_info_routes'
-  
-  get '/rails/info/model', to: 'rails_info/model#index', as: 'rails_info_model'
-  
-  get '/rails/info/data', to: 'rails_info/data#index', as: 'rails_info_data', as: 'rails_info_data'
-  post '/rails/info/data/update_multiple', to: 'rails_info/data#update_multiple', as: 'update_multiple_rails_info_data'
-  
-  get '/rails/info/logs/server' => 'rails_info/logs/server#new', as: 'new_rails_info_server_log'
-  put '/rails/info/logs/server' => 'rails_info/logs/server#update', as: 'rails_info_server_log'
-  
-  get '/rails/info/logs/test/rspec' => 'rails_info/logs/test/rspec#new', as: 'new_rails_info_rspec_log'
-  put '/rails/info/logs/test/rspec' => 'rails_info/logs/test/rspec#update', as: 'rails_info_rspec_log'
-  
-  get '/rails/info/stack_traces/new' => 'rails_info/stack_traces#new', as: 'new_rails_info_stack_trace'
-  post '/rails/info/stack_traces' => 'rails_info/stack_traces#create', as: 'rails_info_stack_trace'
+  mount_rails_info
 end
---
+++ lib/rails_info/routing.rb
@@ -0,0 +1,27 @@
+module ActionDispatch::Routing
+  class Mapper
+    # Includes mount_sextant method for routes. This method is responsible to
+    # generate all needed routes for sextant
+    def mount_rails_info
+      match '/rails/info' => 'rails_info/properties#index', via: :get, via: :get, as: 'rails_info'
+  
+      match '/rails/info/properties' => 'rails_info/properties#index', via: :get, via: :get, as: 'rails_info_properties'
+      match '/rails/info/routes' => 'rails_info/routes#index', via: :get, as: 'rails_info_routes'
+      
+      match '/rails/info/model' => 'rails_info/model#index', via: :get, as: 'rails_info_model'
+      
+      match '/rails/info/data' => 'rails_info/data#index', via: :get, as: 'rails_info_data', via: :get, as: 'rails_info_data'
+      post '/rails/info/data/update_multiple' => 'rails_info/data#update_multiple', via: :post, as: 'rails_update_multiple_rails_info_data'
+      
+      match '/rails/info/logs/server' => 'rails_info/logs/server#new', via: :get, as: 'new_rails_info_server_log'
+      put '/rails/info/logs/server' => 'rails_info/logs/server#update', via: :put, as: 'rails_info_server_log'
+      
+      match '/rails/info/logs/test/rspec' => 'rails_info/logs/test/rspec#new', via: :get, as: 'new_rails_info_rspec_log'
+      put '/rails/info/logs/test/rspec' => 'rails_info/logs/test/rspec#update', via: :put, as: 'rails_info_rspec_log'
+      
+      match '/rails/info/stack_traces/new' => 'rails_info/stack_traces#new', via: :get, as: 'new_rails_info_stack_trace'
+      post '/rails/info/stack_traces' => 'rails_info/stack_traces#create', via: :post, as: 'rails_info_stack_trace'
+    end
+  end
+end
+
