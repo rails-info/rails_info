ccc76bac12bd91ea9d46ddbba221608c491f43aa
mgawlista <mg@reqorder.com>
2012-10-26 11:24:05 +0200

    Extend navigation path in test environment, too

--- lib/rails_info/engine.rb
+++ lib/rails_info/engine.rb
@@ -1,7 +1,7 @@
 module RailsInfo
   class Engine < ::Rails::Engine  
    config.before_initialize do |app|
-      if Rails.env.development?
+      if Rails.env.development? || Rails.env.test?
         SimpleNavigation.config_file_paths << "#{File.expand_path(File.dirname(__FILE__))}/../../config"
       end
     end
